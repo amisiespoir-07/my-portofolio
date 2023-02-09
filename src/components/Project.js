651ba14f91de27fb62841a447344284d999ece6d
amisiespoir-07 <espoirlumbu@gmail.com>
2023-02-09 08:12:26 +0800

    full project

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ export default function Project() {
   useEffect(() => {
     sanityClient
       .fetch(
-        `*[type == "project"]{
+        `*[_type == "project"]{
                 title,
                 date, 
                 place,
@@ -30,48 +30,44 @@ export default function Project() {
         </h2>
         <section className="grid grid-cols-2 gap-8">
           {projectData &&
-            projectData.map((project, index) => (
-              <article>
-                <span
-                  className="relative rounded-lg shadow-xl bg-white p-16"
-                  key={index}
-                >
-                  <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
-                    <a
-                      href={project.link}
-                      alt={project.title}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {project.title}
-                    </a>
-                  </h3>
-                  <div className="text-gry-500 text-xs space-x-4">
-                    <span>
-                      <strong className="font-bold">Finished on</strong>:{" "}
-                      {new Date(project.data).toLocaleDateString()}
-                    </span>
-                    <span>
-                      <strong className="font-bold">Company</strong>:{" "}
-                      {project.place}
-                    </span>
-                    <span>
-                      <strong className="font-bold">Type</strong>:{" "}
-                      {project.projectType}
-                    </span>
-                    <p className="my-6 text-lg text-gray-700 leading-relaxed">
-                      {project.description}
-                    </p>
-                    <a
-                      href={project.link}
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className="text-red-500 font-bold hover:underline hover:text-red-400"
-                    >
-                      View The Projecet{" "}
-                    </a>
-                  </div>
-                </span>
+            projectData.map((project) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished On</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="test-red-500 font-bold hover:underline hover:text-red-400"
+                  >
+                    {" "}
+                    View Project
+                  </a>
+                </div>
               </article>
             ))}
         </section>
